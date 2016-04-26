2cc01d75928acaef590b03b33e7de4e27e63a988
Anatoli Zavialov <mymac@macbook-pro.local>
2016-04-26 19:56:23 +0300

    Initial Commit

---
+++ TablesSearchMultithread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TablesSearchMultithread
+//
+//  Created by My mac on 26.04.16.
+//  Copyright © 2016 Anatolii Zavialov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TablesSearchMultithread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TablesSearchMultithread
+//
+//  Created by My mac on 26.04.16.
+//  Copyright © 2016 Anatolii Zavialov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
